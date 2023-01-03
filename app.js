db9f12d50a8201f79e39a003b2517b94db41e157
Danny Joseph <DjosephNP@gmail.com>
2023-01-03 01:17:25 -0500

    Refactoring code to reduce file redudancy. Organize workflow

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const registerRouter = require('./routes/register.js') 
+const loginRouter = require('./routes/login.js')
+
+const app = express()
+
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({ extended: true}))
+
+app.use(express.static('public'))
+
+app.use(logger)
+
+app.use('/register', registerRouter)
+
+app.use('/login', loginRouter)
+
+app.get('/', (req, res) => 
+{
+  console.log('App is running..')
+  res.statusCode(400)
+  
+})
+
+
+app.get('/dashboard', (req, res) =>
+{
+  console.log('Welcome to the Dashboard')
+  res.render('dashboard')
+})
+
+
+
+function logger(req, res, next) {
+  console.log("The current URL path is: " + req.originalUrl)
+  next()
+}
+
+module.exports = app
--- server.js
+++ server.js
@@ -1,11 +1,6 @@
-const express = require('express')
 const mongoose = require('mongoose')
-const registerRouter = require('./routes/register.js') 
-const loginRouter = require('./routes/login.js')
-
-const app = express()
-
 const uri = 'mongodb+srv://djoseph13:ecV0jmMYY7xW5Bmi@cluster0.b9dbr5a.mongodb.net/?retryWrites=true&w=majority'
+const app = require('./app')
 
 async function connect()
 {
@@ -23,39 +18,7 @@ async function connect()
 
 connect()
 
-app.set('view engine', 'ejs')
-
-app.use(express.urlencoded({ extended: true}))
-
-app.use(express.static('public'))
-
-app.use(logger)
-
-app.use('/register', registerRouter)
-
-app.use('/login', loginRouter)
-
-app.get('/', (req, res) => 
-{
-  console.log('App is running..')
-  
-})
-
-
-app.get('/dashboard', (req, res) =>
-{
-  console.log('Welcome to the Dashboard')
-  res.render('dashboard')
-})
-
-
-
-function logger(req, res, next) {
-  console.log("The current URL path is: " + req.originalUrl)
-  next()
-}
-
 const port = process.env.PORT || 8080
-app.listen(port)
 
-module.exports = app
+app.listen(port);
+
